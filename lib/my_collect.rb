5855fcda4ee32f2620f206a1359c22331df449e1
Kassandra Guerrero <kassandra-g@hotmail.com>
2021-03-16 00:03:51 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,10 @@
-def my_collect(array)
-  my_collect(array) do |x|
-    counter += 1
-  end
+
+def my_collect(collection)
+i = 0
+new_collection = []
+while i < collection.length
+  new_collection << yield(collection[i])
+  i += 1
+end
+new_collection
 end
